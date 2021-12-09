7a1df6c49f8035a0b40630ee9360550770fc14ee
Felix Leiter <felix.leiter00@gmail.com>
2021-12-09 15:41:47 +0100

    Klasse Zombie erstellt

---
+++ src/grind/movables/monster/Zombie.java
@@ -0,0 +1,27 @@
+package grind.movables.monster;
+
+import grind.movables.ISpielfigur;
+import processing.core.PApplet;
+
+public class Zombie extends Monster{
+
+
+    public Zombie(float posX, float posY) {
+        super(posX, posY);
+    }
+
+    @Override
+    public void beiKollision(ISpielfigur figur) {
+
+    }
+
+    @Override
+    public void zeichne(PApplet app) {
+
+    }
+
+    @Override
+    public void bewege() {
+
+    }
+}
