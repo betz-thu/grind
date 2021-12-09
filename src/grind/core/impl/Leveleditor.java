0398038845553eeeaef422c3f4cab89e61eea657
D0kRay <pasi.k11@web.de>
2021-12-09 22:45:41 +0100

    Leveleditormodus hinzugefügt

---
+++ src/grind/core/impl/Leveleditor.java
@@ -0,0 +1,76 @@
+package grind.core.impl;
+
+import grind.core.ISpielmodell;
+import grind.util.Richtung;
+import grind.welt.impl.DummySpielwelt;
+import grind.welt.impl.Spielwelt;
+import processing.core.PApplet;
+
+public class Leveleditor extends PApplet {
+
+    boolean pressed = false;
+
+    ISpielmodell spielmodell;
+
+    public Leveleditor(){
+        this.spielmodell = new Spielmodell(new DummySpielwelt());
+    }
+    @Override
+    public void settings() {
+        size(1200, 800);
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+        eingabe();
+        aktualisiere();
+        zeichne();
+    }
+
+    private void eingabe() {
+
+        if (keyPressed) {
+            if (key == 'a' || keyCode == LEFT) {
+                spielmodell.getFigur().bewege(Richtung.W);
+            } else if (key == 'w' || keyCode == UP) {
+                spielmodell.getFigur().bewege(Richtung.N);
+            } else if (key == 's' || keyCode == DOWN) {
+                spielmodell.getFigur().bewege(Richtung.S);
+            } else if (key == 'd' || keyCode == RIGHT) {
+                spielmodell.getFigur().bewege(Richtung.O);
+            }
+        }
+
+        //F12 neue Szene
+        if (keyPressed && !pressed){
+            if (keyCode == 123) {
+                pressed = true;
+            }
+        } else if(!keyPressed && pressed){
+            pressed = false;
+            System.out.println("F12");
+            spielmodell.setSzeneNr(spielmodell.getSzeneNr()+1);
+            spielmodell.betreteSzene(spielmodell.getSzeneNr());
+        }
+    }
+
+    private void aktualisiere() {
+        spielmodell.bewege();
+
+    }
+
+    private void zeichne() {
+        spielmodell.zeichne(this);
+    }
+
+    @Override
+    public void mousePressed() {
+        // nur notwendig, falls Maus benötigt wird
+    }
+
+}
--- src/grind/welt/impl/Leveleditor.java
+++
@@ -1,7 +0,0 @@
-package grind.welt.impl;
-
-public class Leveleditor {
-    public static void main(String[] args) {
-
-    }
-}
