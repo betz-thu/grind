ed3175b609e2ee8a15b2181b0df16df3dc4a0eda
D0kRay <pasi.k11@web.de>
2021-12-15 16:35:24 +0100

    Gson Library hinzugefügt

--- src/grind/core/impl/DateiService.java
+++ src/grind/core/impl/DateiService.java
@@ -1,6 +1,12 @@
 package grind.core.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class DateiService {
+
+    GsonBuilder gsonbuilder = new GsonBuilder();
+    Gson gson = gsonbuilder.create();
 
 
 }
