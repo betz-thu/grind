ab2db1430c9a1838bb4bb01f0a0817898d8f4351
Fabian K <f-klueber@t-online.de>
2021-12-17 14:18:17 +0100

    Die Klasse wurde um einen weiteren Konstruktor ergänzt

--- src/grind/movables/monster/Monster.java
+++ src/grind/movables/monster/Monster.java
@@ -3,17 +3,30 @@ package grind.movables.monster;
 import grind.core.ISpielmodell;
 import grind.movables.ISpielfigur;
 import grind.movables.impl.Movable;
+import grind.util.Richtung;
 
 public abstract class Monster extends Movable implements IMonster {
     private int lebensenergie;
     ISpielmodell spielmodell;
 
 
-
-
+    /**
+     * Konstruktor 1
+     * @param posX
+     * @param posY
+     */
     public Monster(float posX, float posY) {
-
         super(posX, posY);
+    }
+
+    /**
+     * Konstruktor 2 wenn die Ausrichtung(N,S,W,O) für die Fortbewegung relevant ist
+     * @param posX
+     * @param posY
+     * @param ausrichtung
+     */
+    public Monster(float posX, float posY, Richtung ausrichtung) {
+        super(posX,posY,ausrichtung);
     }
 
     @Override
@@ -29,6 +42,7 @@ public abstract class Monster extends Movable implements IMonster {
     public ISpielmodell getSpielmodell() {
         return spielmodell;
     }
+
     @Override
     public void setSpielmodell(ISpielmodell spielmodell) {
         this.spielmodell = spielmodell;
