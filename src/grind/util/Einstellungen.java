20e534592ddb2058512689c46f7737c400098f40
Fabian K <f-klueber@t-online.de>
2021-12-17 14:21:19 +0100

    Hinzugefügt: GROESSE_FEUERMONSTER, GROESSE_FEUERBALL

--- src/grind/util/Einstellungen.java
+++ src/grind/util/Einstellungen.java
@@ -10,4 +10,5 @@ public class Einstellungen {
     public static final int GROESSE_GEIST = 20;
     public static final int GROESSE_DORNPFLANZE = 40;
     public static final int GROESSE_FEUERBALL = 10;
+    public static final int GROESSE_FEUERMONSTER = 30;
 }
