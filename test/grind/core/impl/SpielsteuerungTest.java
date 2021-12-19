fc96d27c2dd1347f0db32f0cfe4a32d4ed89d167
Fabian K <f-klueber@t-online.de>
2021-12-19 20:06:05 +0100

    Klasse: SpielsteuerungTest

---
+++ test/grind/core/impl/SpielsteuerungTest.java
@@ -0,0 +1,41 @@
+package grind.core.impl;
+
+import grind.util.Einstellungen;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpielsteuerungTest {
+    Spielsteuerung spielsteuerung;
+
+    @BeforeEach
+    void setUp() {
+        spielsteuerung= new Spielsteuerung();
+    }
+
+    @Test
+    void settings() {
+    }
+
+    @Test
+    void setup() {
+    }
+
+    @Test
+    void draw() {
+    }
+
+
+    @Test
+    void getKachelByCoordinates() {
+    }
+
+    @Test
+    void isSpielfeldrand() {
+    }
+
+    @Test
+    void isErlaubteKoordinate() {
+    }
+}
