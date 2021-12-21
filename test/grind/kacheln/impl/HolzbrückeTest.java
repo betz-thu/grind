039a6ea71de94e82e163fb2fa5023349606bce3a
yoasb <yberhe@mail.hs-ulm.de>
2021-12-21 16:24:55 +0100

    Update Hindernisse/Betretbar Tests

---
+++ test/grind/kacheln/impl/HolzbrückeTest.java
@@ -0,0 +1,25 @@
+package grind.kacheln.impl;
+
+import grind.movables.impl.Spielfigur;
+import org.junit.Assert;
+import org.junit.Test;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+import static org.junit.Assert.*;
+
+public class HolzbrückeTest {
+
+    Holzbrücke holzbrücke = new Holzbrücke();
+
+    @Test
+    public void istBetretbar() {
+        Assert.assertTrue(holzbrücke.istBetretbar());
+    }
+
+    @Test
+    public void istHindernis() {
+        Assert.assertFalse(holzbrücke.istHindernis());
+    }
+
+}
---
+++ test/grind/kacheln/impl/WegTest.java
@@ -0,0 +1,25 @@
+package grind.kacheln.impl;
+
+import grind.movables.impl.Spielfigur;
+import org.junit.Assert;
+import org.junit.Test;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+import static org.junit.Assert.*;
+
+public class WegTest {
+
+    Weg weg = new Weg();
+
+    @Test
+    public void istBetretbar() {
+        Assert.assertTrue(weg.istBetretbar());
+    }
+
+    @Test
+    public void istHindernis() {
+        Assert.assertFalse(weg.istHindernis());
+    }
+
+}
