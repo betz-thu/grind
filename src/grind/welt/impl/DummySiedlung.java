131b6e0dd274d4ef15503ce365190f16480edb91
NicoB4 <nicobaldin@aol.de>
2022-01-04 19:22:36 +0100

    Shop Hintergrund

---
+++ src/grind/welt/impl/DummySiedlung.java
@@ -0,0 +1,20 @@
+package grind.welt.impl;
+
+import grind.welt.ILevel;
+import grind.welt.ISiedlung;
+import processing.core.PApplet;
+
+public class DummySiedlung implements ISiedlung {
+
+
+    @Override
+    public void zeichne(PApplet app) {
+        app.background(0,0,0);
+        app.
+    }
+
+    @Override
+    public ILevel getLevel() {
+        return null;
+    }
+}
