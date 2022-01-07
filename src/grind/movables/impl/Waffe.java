cf9b79c2cbf3ab449f9f30113039cd3eb5aeeb68
Margazim <glorie98@googlemail.com>
2022-01-07 18:38:21 +0100

    Zeilenumbruch

--- src/grind/movables/impl/Waffe.java
+++ src/grind/movables/impl/Waffe.java
@@ -24,7 +24,7 @@ public abstract class Waffe extends Gegenstand{
 
     @Override
     public String toString(){
-        return "Schaden: "+ getSchaden()+", Stufe: " + getStufe() ;
+        return "Schaden: "+ getSchaden()+"\nStufe: " + getStufe() ;
     }
 
 
