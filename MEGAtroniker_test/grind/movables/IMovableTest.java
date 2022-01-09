b172f2f3ae7bc53544012e88640b9a8d0c774734
Fabian K <f-klueber@t-online.de>
2022-01-09 14:30:18 +0100

    Test-Ordner-MEGAtroniker erstellt

---
+++ MEGAtroniker_test/grind/movables/IMovableTest.java
@@ -0,0 +1,45 @@
+package grind.movables;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IMovableTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getPosX() {
+    }
+
+    @Test
+    public void getPosY() {
+    }
+
+    @Test
+    public void getGroesse() {
+    }
+
+    @Test
+    public void setPosition() {
+    }
+
+    @Test
+    public void zeichne() {
+    }
+
+    @Test
+    public void bewege() {
+    }
+
+    @Test
+    public void getAusrichtung() {
+    }
+
+    @Test
+    public void setAusrichtung() {
+    }
+}
